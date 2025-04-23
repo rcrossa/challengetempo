c2fe8d533283a128bdeae0e1a7c5c0676aa20d76
Roberto Rossa <rcrossa@hotmail.com>
2025-04-23 10:16:35 -0300

    Refactor.

--- src/main/java/com/tempo/challenge/dtos/TransactionDTO.java
+++ src/main/java/com/tempo/challenge/dtos/TransactionDTO.java
@@ -3,9 +3,7 @@ package com.tempo.challenge.dtos;
 import com.tempo.challenge.validation.ValidString;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
+import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
 
@@ -16,15 +14,20 @@ import java.time.LocalDate;
 @Builder
 @AllArgsConstructor
 @Table(name = "transactions")
+@Getter
+@Setter
 public class TransactionDTO {
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
     @NotNull(message = "The amount is required")
     @Min(value = 1, message = "The amount must be greater than 0")
     private Integer amount;
+
 
     @NotBlank(message = "The merchant is required")
     @ValidString(message = "The merchant must contain only letters, numbers, and spaces")
@@ -39,43 +42,4 @@ public class TransactionDTO {
     @DateTimeFormat(pattern = "dd-MM-yyyy")
     private LocalDate transactionDate;
 
-    public Long getId() {
-        return id;
-    }
-
-    public @NotNull(message = "The amount is required") @Min(value = 1, message = "The amount must be greater than 0") Integer getAmount() {
-        return amount;
-    }
-
-    public @NotBlank(message = "The merchant is required") String getMerchant() {
-        return merchant;
-    }
-
-    public @NotBlank(message = "The name of the client is required") String getClientName() {
-        return clientName;
-    }
-
-    public @NotNull(message = "The date of the transaction is required") @PastOrPresent(message = "The date of the transaction must be in the past or present") LocalDate getTransactionDate() {
-        return transactionDate;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setAmount(@NotNull(message = "The amount is required") @Min(value = 1, message = "The amount must be greater than 0") Integer amount) {
-        this.amount = amount;
-    }
-
-    public void setMerchant(@NotBlank(message = "The merchant is required") String merchant) {
-        this.merchant = merchant;
-    }
-
-    public void setClientName(@NotBlank(message = "The name of the client is required") String clientName) {
-        this.clientName = clientName;
-    }
-
-    public void setTransactionDate(@NotNull(message = "The date of the transaction is required") @PastOrPresent(message = "The date of the transaction must be in the past or present") LocalDate transactionDate) {
-        this.transactionDate = transactionDate;
-    }
 }
